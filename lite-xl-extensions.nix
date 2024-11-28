5605439e388844305119f5534a90a61a4e0336db
Gabriele <gmercolino2003@gmail.com>
2024-11-28 10:19:53 +0100

    feat: added more plugins

--- lite-xl-extensions.nix
+++ lite-xl-extensions.nix
@@ -5,12 +5,14 @@ let
     {
       name,
       src,
-      targetDir,
+      targetDir ? "lite-xl/plugins/${name}",
     }:
     stdenv.mkDerivation rec {
       inherit name src;
 
       passthru.targetDir = targetDir;
+
+      phases = [ "installPhase" ];
 
       installPhase = ''
         mkdir -p $out/share/${name}
@@ -22,7 +24,7 @@ let
     {
       name,
       src,
-      targetDir,
+      targetDir ? "lite-xl/plugins/${name}",
     }:
     stdenv.mkDerivation rec {
       inherit name src;
@@ -66,7 +68,42 @@ in
       url = "https://raw.githubusercontent.com/lite-xl/lite-xl-plugins/refs/heads/master/plugins/autoinsert.lua";
       sha256 = "sha256-qbWsR0L3Fb3pVVe9BQ40NffUpiY7IXXxJ6J1nF//WBk=";
     };
-    targetDir = "lite-xl/plugins/autoinsert";
+  };
+
+  colorpreview = mkSingleScriptExtension {
+    name = "colorpreview";
+    src = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/lite-xl/lite-xl-plugins/refs/heads/master/plugins/colorpreview.lua";
+      sha256 = "sha256-d7cLFPllpwLKWIek1zO88KNMb/aBjuZE2jWoOXziZe8=";
+    };
+  };
+
+  code-plus = mkExtension {
+    name = "code-plus";
+    src = pkgs.fetchFromGitHub {
+      owner = "chqs-git";
+      repo = "code-plus";
+      rev = "main";
+      sha256 = "sha256-uE5kATb8hDZOjiTGO3F3xs3ccRtjkqSz8Dm6pST0iWQ=";
+    };
+  };
+
+  console = mkExtension {
+    name = "console";
+    src = pkgs.fetchFromGitHub {
+      owner = "lite-xl";
+      repo = "console";
+      rev = "master";
+      sha256 = "sha256-ihU5du2+oEoltcDey88yKWAZxELXMaOZg7xSddkFEls=";
+    };
+  };
+
+  gitstatus = mkSingleScriptExtension {
+    name = "gitstatus";
+    src = pkgs.fetchurl {
+      url = "https://raw.githubusercontent.com/lite-xl/lite-xl-plugins/refs/heads/master/plugins/gitstatus.lua";
+      sha256 = "sha256-i4ZSjFXcqMGsABJac4movtnDY6BZ0m7xa1cnAa3VSoE=";
+    };
   };
 
 }
