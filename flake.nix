77ad93766e6ddc4fcc387e196fa1924a25b41cf0
Gabriele <gmercolino2003@gmail.com>
2025-02-25 10:18:28 +0100

    chore: using new lsp system

--- flake.nix
+++ flake.nix
@@ -5,32 +5,29 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   };
 
-  outputs =
-    {
-      self,
-      nixpkgs,
-      ...
-    }:
-    let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in
-    {
-      overlay = final: prev: {
-        lite-xl-plugins = final.callPackage ./plugins.nix { };
-        lite-xl-lsp = final.callPackage ./lspServers.nix { };
-      };
-
-      packages.${system} = {
-        lite-xl-plugins = pkgs.callPackage ./plugins.nix { };
-        lite-xl-lsp = pkgs.callPackage ./lspServers.nix { };
-      };
-
-      homeManagerModules = {
-        lite-xl = import ./module.nix;
-        default = self.homeManagerModules.lite-xl;
-      };
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs {
+      inherit system;
     };
+  in {
+    overlay = final: prev: {
+      lite-xl-plugins = final.callPackage ./plugins.nix {};
+      lite-xl-lsp = final.callPackage ./lsp {};
+    };
+
+    packages.${system} = {
+      lite-xl-plugins = pkgs.callPackage ./plugins.nix {};
+      lite-xl-lsp = pkgs.callPackage ./lsp {};
+    };
+
+    homeManagerModules = {
+      lite-xl = import ./module.nix;
+      default = self.homeManagerModules.lite-xl;
+    };
+  };
 }
