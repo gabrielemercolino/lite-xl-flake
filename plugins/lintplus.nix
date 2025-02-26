33508cb80eb1736f23a5bc7166556e1419f294d9
Gabriele <gmercolino2003@gmail.com>
2025-02-26 23:11:34 +0100

    feat: added lintplus

---
+++ plugins/lintplus.nix
@@ -0,0 +1,22 @@
+{
+  pkgs,
+  stdenv,
+}:
+stdenv.mkDerivation rec {
+  name = "lintplus";
+
+  passthru.targetDir = "lite-xl/plugins/${name}";
+  dontUnpack = true;
+
+  src = pkgs.fetchFromGitHub {
+    owner = "liquidev";
+    repo = "lintplus";
+    rev = "master";
+    sha256 = "sha256-CRfv6eFY4dZ9J937NHK+HE+e7fLkCH+cE6MQgn2OxNg=";
+  };
+
+  installPhase = ''
+    mkdir -p $out/share/${name}
+    cp -r $src/* $out/share/${name}
+  '';
+}
